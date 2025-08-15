44318c5a54329bb7384fd7cc165aca0dbaa5d535
asyaisbil <asyaisbill@gmail.com>
2025-08-15 18:13:01 +0300

    Project-1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,34 +2,73 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(area(-1.0));
     }
 
     public static boolean shouldWakeUp(boolean isBarking, int clock) {
-        if(clock <= 23 && clock >= 0) {
-            if (clock > 20 && clock < 8) {
+        if(isBarking == false) {
+            return false;
+        }
+        else {
+            if (clock <= 23 && clock >= 0) {
+                if (clock > 20 || clock < 8) {
+                    return true;
+                } else {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        }
+    }
+
+
+    public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
+       if(firstAge >= 13 && firstAge <= 19){
+           return true;
+       } else if(secondAge >= 13 && secondAge <= 19){
+           return true;
+       } else if(thirdAge >= 13 && thirdAge <= 19){
+           return true;
+       } else {
+           return false;
+       }
+    }
+
+
+    public static boolean isCatPlaying(boolean isSummer, int temp) {
+        if(isSummer==false){
+            if(temp >= 25 && temp <= 35){
                 return true;
             } else {
                 return false;
             }
         } else {
-            return false;
+            if(temp >= 25 && temp <= 45){
+                return true;
+            } else {
+                return false;
+            }
         }
     }
 
-    public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
-       
-    }
-
-    public static boolean isCatPlaying(boolean isSummer, int temp) {
-     
-    }
 
     public static double area(double width, double height) {
-       
+       if(width < 0 || height < 0){
+           return -1;
+       } else {
+           return width*height;
+       }
     }
 
+
     public static double area(double radius) {
-       
+       if(radius < 0){
+           return -1;
+       } else {
+           double result = radius*radius*Math.PI;
+           result = Math.round(result * 100000.0) / 100000.0;
+           return result;
+       }
     }
 }
